f4e39fcca999c607a1a0887eb91582b4354d8468
douglas <douglasmwangi94@gmail.com>
2022-09-08 03:01:31 +0300

    adding the the body to services tab

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function bigImg(x) {
   let cards = document.getElementById("cards")
   let message = document.getElementById("message")
   message.addEventListener("click",()=>{
-    cards.innerHTML="<h1>Messages</h1>"
+    cards.innerHTML="<h1>Messages Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h1>"
   })
 
   // adding click event to the services page
